5ad67fbf1cfabe039e3735ce1522c5b4920363fb
Samuel Márquez Estrada <samuelmares381@gmail.com>
2026-05-06 14:35:13 -0600

    pequeñas modificaciones a main

--- main.cpp
+++ main.cpp
@@ -5,3 +5,13 @@ Emiliano Panuncio y Samuel Márquez
 Simulación de un portafolio con activos financieros usando movimiento Browniano
 */
 
+#include "AnalizadorPortafolio.h"
+#include "RegistroSimulaciones.h"
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void ExportarCSV(vector<double> resultados){
+    
+};
